905b87dccf5387516745f5fdc4d892d07e29e979
Guilherme Sehn <ghsehn@gmail.com>
2016-02-28 12:45:32 -0300

    implement deposits and withdrawals

--- lib/bank_account.ex
+++ lib/bank_account.ex
@@ -1,17 +1,27 @@
 defmodule BankAccount do
   def start do
-    await
+    await(0)
   end
 
-  def await do
+  def await(balance) do
     receive do
-      {:check_balance, pid} -> send_balance(pid)
+      {:check_balance, pid} -> send_balance(pid, balance)
+      {:deposit, amount}    -> balance = deposit(balance, amount)
+      {:withdraw, amount}   -> balance = withdraw(balance, amount)
     end
 
-    await
+    await(balance)
   end
 
-  defp send_balance(pid) do
-    send(pid, {:balance, 0})
+  defp send_balance(pid, balance) do
+    send(pid, {:balance, balance})
+  end
+
+  defp deposit(balance, amount) do
+    balance + amount
+  end
+
+  defp withdraw(balance, amount) do
+    balance - amount
   end
 end
--- test/bank_account_test.exs
+++ test/bank_account_test.exs
@@ -7,4 +7,19 @@ defmodule BankAccountTest do
     send(account, {:check_balance, self})
     assert_receive {:balance, 0}
   end
+
+  test "has balance incremented by the amount of a deposit" do
+    account = spawn_link(BankAccount, :start, [])
+    send(account, {:deposit, 10})
+    send(account, {:check_balance, self})
+    assert_receive {:balance, 10}
+  end
+
+  test "has balance decreased by the amount of a withdrawal" do
+    account = spawn_link(BankAccount, :start, [])
+    send(account, {:deposit, 50})
+    send(account, {:withdraw, 20})
+    send(account, {:check_balance, self})
+    assert_receive {:balance, 30}
+  end
 end
